78b0f0d3a5531cdf586e8ba90e547a0356d55151
Asad-Zaidi <syedasad1410@gmail.com>
2023-11-30 20:58:22 +0500

    React Calculator

--- react-calculator/src/Component/Calculator.jsx
+++ react-calculator/src/Component/Calculator.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import Header from './Header'
 
 export default function Calculator() {
-  return (
-    <>
-    <Header/>
-    <div>Calculator</div>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <div>Calculator</div>
+        </>
+    )
 }
