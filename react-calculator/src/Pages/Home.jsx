12286e8f49b3dadfbab8680b09a6e6b8a6a6a3d8
Asad-Zaidi <syedasad1410@gmail.com>
2023-12-06 21:40:42 +0500

    Routes set

--- react-calculator/src/Pages/Home.jsx
+++ react-calculator/src/Pages/Home.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
 import Head from '../Nav-Bar/Header'
+import gif from '../Icons/mathBrain.gif'
+import Math_GIF from'../Icons/Math_GIF.gif'
+import '../Style/Home.css'
 
-export default function Home() {
-    return (
-        <>
-            <Head />
-            <h1>Home</h1>
-        </>
-    )
-}
+const Home = () => (
+    <body>
+        <Head/>
+        <div className="MathGif">
+            <img src={Math_GIF} alt="math"/>
+            </div>
+        <section className="home-container">
+            <div>
+                <h2 className="subtitle">Welcome to our page!</h2>
+                <div className="description">
+                    <p className="text">
+                        <b>Math magicians</b>
+                        {' '}
+                        is a website for all fans of mathematics.
+                        <br />
+                        It is a Single Page App (SPA) that allows users to:
+                    </p>
+                    <ul>
+                        <li className="listItem">Make simple calculations.</li>
+                        <li className="listItem">Read a random math-related quote.</li>
+                    </ul>
+                    <p>
+                        The app is developed using
+                        {' '}
+                        <i>React,</i>
+                        {' '}
+                        and styled through the
+                        {' '}
+                        <i>React basic styling.</i>
+                        {' '}
+                        Navigation is made possible through
+                        {' '}
+                        <i>React Router.</i>
+                    </p>
+                </div>
+            </div>
+            <div className="home-img">
+                <img src={gif} alt="math" />
+            </div>
+        </section>
+    </body>
+);
+
+export default Home;
--- react-calculator/src/Pages/Qoute.jsx
+++ react-calculator/src/Pages/Qoute.jsx
@@ -1,3 +1,50 @@
+// import React, { useState, useEffect } from 'react';
+// import Head from '../Nav-Bar/Header';
+// import './Qoute.css';
+
+// export default function Qoute() {
+//     const [quotes, setQuotes] = useState([]);
+//     const [currentQuoteIndex, setCurrentQuoteIndex] = useState(0);
+
+//     useEffect(() => {
+//         const fetchQuotes = async () => {
+//             try {
+//                 const response = await fetch('https://type.fit/api/quotes');
+//                 const data = await response.json();
+//                 setQuotes(data);
+//             } catch (error) {
+//                 console.error('Error fetching quotes:', error);
+//             }
+//         };
+
+//         fetchQuotes();
+//     }, []);
+
+//     useEffect(() => {
+//         const intervalId = setInterval(() => {
+//             setCurrentQuoteIndex((prevIndex) => (prevIndex + 3) % quotes.length);
+//         }, 100000);
+
+//         return () => clearInterval(intervalId);
+//     }, [quotes]);
+
+//     return (
+//         <>
+//             <Head />
+//             <div className="quote-container">
+//                 <div className="quote-wrapper">
+//                     {quotes.length > 0 && (
+//                         <blockquote className="quote">
+//                             <p>{quotes[currentQuoteIndex].text}</p>
+//                             <footer>- {quotes[currentQuoteIndex].author || 'Unknown'}</footer>
+//                         </blockquote>
+//                     )}
+//                 </div>
+//             </div>
+//         </>
+//     );
+// }
+
 import React, { useState, useEffect } from 'react';
 import Head from '../Nav-Bar/Header';
 import './Qoute.css';
@@ -22,11 +69,15 @@ export default function Qoute() {
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setCurrentQuoteIndex((prevIndex) => (prevIndex + 3) % quotes.length);
-        }, 100000);
+            setCurrentQuoteIndex((prevIndex) => (prevIndex + 1) % quotes.length);
+        }, 10000);
 
         return () => clearInterval(intervalId);
     }, [quotes]);
+
+    const handleGenerateQuote = () => {
+        setCurrentQuoteIndex((prevIndex) => (prevIndex + 1) % quotes.length);
+    };
 
     return (
         <>
@@ -35,10 +86,12 @@ export default function Qoute() {
                 <div className="quote-wrapper">
                     {quotes.length > 0 && (
                         <blockquote className="quote">
-                            <p>{quotes[currentQuoteIndex].text}</p>
-                            <footer>- {quotes[currentQuoteIndex].author || 'Unknown'}</footer>
+                            <p>"{quotes[currentQuoteIndex].text}"</p>
                         </blockquote>
                     )}
+                    <button className="generate" onClick={handleGenerateQuote}>
+                        Generate New Quote
+                    </button>
                 </div>
             </div>
         </>
