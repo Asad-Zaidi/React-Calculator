00f67ea08ba84797610ca4389a6592e21e64af01
Asad-Zaidi <syedasad1410@gmail.com>
2023-12-07 00:25:08 +0500

    README Updated

--- react-calculator/src/Component/Calculator.jsx
+++ react-calculator/src/Component/Calculator.jsx
@@ -27,8 +27,8 @@ const Calculator = () => {
         return () => {
             document.removeEventListener('keydown', handleKeyPress);
         };
-    }, []); 
-    
+    }, []);
+
 
 
 
@@ -50,44 +50,39 @@ const Calculator = () => {
     };
 
     return (
-        
-            
-                <div className="calculator-container">
-                    <CalculatorInput value={input} />
-                    <div className="buttons-container">
-                        <CalculatorButton className="top-row" onClick={handleClear}>AC</CalculatorButton>
-                        <CalculatorButton className="top-row" onClick={() => handleCalculatorButtonClick('+/-')}> +/- </CalculatorButton>
-                        <CalculatorButton className="top-row" onClick={() => handleCalculatorButtonClick('%')}> % </CalculatorButton>
-                        <CalculatorButton className="operator" onClick={() => handleCalculatorButtonClick('/')}> ÷ </CalculatorButton>
-                    </div>
-
-                    <div className="CalculatorButton-row">
-                        <CalculatorButton onClick={() => handleCalculatorButtonClick('1')}>1</CalculatorButton>
-                        <CalculatorButton onClick={() => handleCalculatorButtonClick('2')}>2</CalculatorButton>
-                        <CalculatorButton onClick={() => handleCalculatorButtonClick('3')}>3</CalculatorButton>
-                        <CalculatorButton className="operator" onClick={() => handleCalculatorButtonClick('+')}>+</CalculatorButton>
-                    </div>
-                    <div className="CalculatorButton-row">
-                        <CalculatorButton onClick={() => handleCalculatorButtonClick('4')}>4</CalculatorButton>
-                        <CalculatorButton onClick={() => handleCalculatorButtonClick('5')}>5</CalculatorButton>
-                        <CalculatorButton onClick={() => handleCalculatorButtonClick('6')}>6</CalculatorButton>
-                        <CalculatorButton className="operator" onClick={() => handleCalculatorButtonClick('-')}>-</CalculatorButton>
-                    </div>
-                    <div className="CalculatorButton-row">
-                        <CalculatorButton onClick={() => handleCalculatorButtonClick('7')}>7</CalculatorButton>
-                        <CalculatorButton onClick={() => handleCalculatorButtonClick('8')}>8</CalculatorButton>
-                        <CalculatorButton onClick={() => handleCalculatorButtonClick('9')}>9</CalculatorButton>
-                        <CalculatorButton className="operator" onClick={() => handleCalculatorButtonClick('*')}>x</CalculatorButton>
-                    </div>
-                    <div className="CalculatorButton-row">
-                        <CalculatorButton onClick={() => handleCalculatorButtonClick('0')}>0</CalculatorButton>
-                        <CalculatorButton onClick={() => handleCalculatorButtonClick('00')}>00</CalculatorButton>
-                        <CalculatorButton onClick={() => handleCalculatorButtonClick('.')}>.</CalculatorButton>
-                        <CalculatorButton className="operator" onClick={handleCalculate}> = </CalculatorButton>
-                    </div>
-                </div>
-            
-        
+        <div className="calculator-container">
+            <CalculatorInput value={input} />
+            <div className="buttons-container">
+                <CalculatorButton className="top-row" onClick={handleClear}>AC</CalculatorButton>
+                <CalculatorButton className="top-row" onClick={() => handleCalculatorButtonClick('+/-')}> +/- </CalculatorButton>
+                <CalculatorButton className="top-row" onClick={() => handleCalculatorButtonClick('%')}> % </CalculatorButton>
+                <CalculatorButton className="operator" onClick={() => handleCalculatorButtonClick('/')}> ÷ </CalculatorButton>
+            </div>
+            <div className="CalculatorButton-row">
+                <CalculatorButton onClick={() => handleCalculatorButtonClick('1')}>1</CalculatorButton>
+                <CalculatorButton onClick={() => handleCalculatorButtonClick('2')}>2</CalculatorButton>
+                <CalculatorButton onClick={() => handleCalculatorButtonClick('3')}>3</CalculatorButton>
+                <CalculatorButton className="operator" onClick={() => handleCalculatorButtonClick('+')}>+</CalculatorButton>
+            </div>
+            <div className="CalculatorButton-row">
+                <CalculatorButton onClick={() => handleCalculatorButtonClick('4')}>4</CalculatorButton>
+                <CalculatorButton onClick={() => handleCalculatorButtonClick('5')}>5</CalculatorButton>
+                <CalculatorButton onClick={() => handleCalculatorButtonClick('6')}>6</CalculatorButton>
+                <CalculatorButton className="operator" onClick={() => handleCalculatorButtonClick('-')}>-</CalculatorButton>
+            </div>
+            <div className="CalculatorButton-row">
+                <CalculatorButton onClick={() => handleCalculatorButtonClick('7')}>7</CalculatorButton>
+                <CalculatorButton onClick={() => handleCalculatorButtonClick('8')}>8</CalculatorButton>
+                <CalculatorButton onClick={() => handleCalculatorButtonClick('9')}>9</CalculatorButton>
+                <CalculatorButton className="operator" onClick={() => handleCalculatorButtonClick('*')}>x</CalculatorButton>
+            </div>
+            <div className="CalculatorButton-row">
+                <CalculatorButton onClick={() => handleCalculatorButtonClick('0')}>0</CalculatorButton>
+                <CalculatorButton onClick={() => handleCalculatorButtonClick('00')}>00</CalculatorButton>
+                <CalculatorButton onClick={() => handleCalculatorButtonClick('.')}>.</CalculatorButton>
+                <CalculatorButton className="operator" onClick={handleCalculate}> = </CalculatorButton>
+            </div>
+        </div>
     );
 };
 
